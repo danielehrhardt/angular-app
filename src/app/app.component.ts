611557c1484f6a36034031ba4bff23bcd807a0be
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2023-03-27 00:13:33 +0200

    Added Image and Sample Text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'Hallo YouTube';
+
+  ngOnInit() {
+    console.log('Hallo ngOnInit');
+    setInterval(() => {
+      this.title = 'Hallo YouTube ' + new Date().getSeconds();
+      console.log('Hallo setInterval', this.title);
+    }, 1000);
+  }
+
+  ngAfterViewInit() {
+    console.log('Hallo ngAfterViewInit');
+  }
+
 }
+
